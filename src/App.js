398db035d0ef9f24fcdb47b99a766140966681c4
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-10-11 15:50:12 +0800

    Fix some fetch that not have catch follow it

--- src/App.js
+++ src/App.js
@@ -71,6 +71,7 @@ class App extends Component {
           })
           .then(res => res.json())
           .then(count => this.setState(Object.assign(this.state.user, {entries: count})))
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFacaLocation(response));
       })
